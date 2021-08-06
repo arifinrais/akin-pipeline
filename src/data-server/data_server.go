8a1671a1722d7125e310d3f88b2dc6cb84cd169a
Arifin Rais <44962398+arifinrais@users.noreply.github.com>
2021-08-06 23:06:09 +0700

    [DEV] data-server

--- src/data-server/data_server.go
+++ src/data-server/data_server.go
@@ -6,7 +6,9 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/arifinrais/akin-pipeline/src/data-server/models"
+	"data-server/helper"
+	"data-server/models"
+
 	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -78,88 +80,6 @@ func getBook(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(book)
 }
 
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var book models.Book
-
-	// we decode our body request params
-	_ = json.NewDecoder(r.Body).Decode(&book)
-
-	// insert our book model.
-	result, err := collection.InsertOne(context.TODO(), book)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	json.NewEncoder(w).Encode(result)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	var params = mux.Vars(r)
-
-	//Get id from parameters
-	id, _ := primitive.ObjectIDFromHex(params["id"])
-
-	var book models.Book
-
-	// Create filter
-	filter := bson.M{"_id": id}
-
-	// Read update model from body request
-	_ = json.NewDecoder(r.Body).Decode(&book)
-
-	// prepare update model.
-	update := bson.D{
-		{"$set", bson.D{
-			{"isbn", book.Isbn},
-			{"title", book.Title},
-			{"author", bson.D{
-				{"firstname", book.Author.FirstName},
-				{"lastname", book.Author.LastName},
-			}},
-		}},
-	}
-
-	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&book)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	book.ID = id
-
-	json.NewEncoder(w).Encode(book)
-}
-
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	// Set header
-	w.Header().Set("Content-Type", "application/json")
-
-	// get params
-	var params = mux.Vars(r)
-
-	// string to primitve.ObjectID
-	id, err := primitive.ObjectIDFromHex(params["id"])
-
-	// prepare filter.
-	filter := bson.M{"_id": id}
-
-	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
-
-	if err != nil {
-		helper.GetError(err, w)
-		return
-	}
-
-	json.NewEncoder(w).Encode(deleteResult)
-}
-
 // var client *mongo.Client
 
 func main() {
@@ -168,9 +88,6 @@ func main() {
 
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
 	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
 
 	config := helper.GetConfiguration()
 	log.Fatal(http.ListenAndServe(config.Port, r))
--- src/data-server/go.mod
+++ src/data-server/go.mod
@@ -1,8 +1,9 @@
-module example.com/hello
+module data-server
 
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	go.mongodb.org/mongo-driver v1.7.1 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.7.1
 )
